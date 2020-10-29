f8a45eff85ab81bd3211725f4a61866386ce8a8d
zx-Nebula <73566418+zx-Nebula@users.noreply.github.com>
2020-10-29 16:42:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,3 +1,3 @@
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='zx_Nebula'' package/lean/default-settings/files/zzz-default-settings
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
